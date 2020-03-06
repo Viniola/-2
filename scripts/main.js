fb58ec80dabc02146144981adce43bed1bc89319
Viniola <61869285+Viniola@users.noreply.github.com>
2020-03-06 13:37:24 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+﻿var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/fatezerogilgamesh.png.jpg') {
+      myImage.setAttribute ('src','images/fategrandorder.png.jpg');
+    } else {
+      myImage.setAttribute ('src','images/fatezerogilgamesh.png.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h5');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Добро пожаловать, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Добро пожаловать, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
